f5b72935242219d40d193be63237f3967ad8f453
nicholma <nicholma@tcd.ie>
2013-02-01 15:03:15 +0000

    Added some stuff to my folder

---
+++ nicholma/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title></title>
+<link rel="stylesheet" type="text/css" href="theStyleSheet.css">
+
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2J5mFUg4TvrqmCVvTznGkcXh3ENf6-zk&sensor=true"></script>
+    <script src="theJavaScript.js"></script>
+
+
+<body>
+<div id="headerMenu">
+		<br><p id=headerOption><a href="index.php">Home</a> bbbbb ccccc ddddd</p><br>
+</div>
+
+
+<div id="sideMenu">
+		<div class="sideMenuOption">11111</div>
+		<div class="sideMenuOption">22222</div>
+		<div class="sideMenuOption">33333</div>
+		<div class="sideMenuOption">44444</div>
+</div>
+
+
+<div id="mainContent">
+	<form action="admin.php">
+    <input type="submit" value="Create new Crawl">
+    </form>   
+</div>
+
+</body>
+</html>
